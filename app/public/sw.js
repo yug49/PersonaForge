b4009e29b05b97303b414310a7791987a755cafd
yug49 <148035793+yug49@users.noreply.github.com>
2025-09-02 15:52:27 +0530

    updated git ignore

---
+++ app/public/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'personaforge-v1';
+const urlsToCache = [
+  '/',
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
